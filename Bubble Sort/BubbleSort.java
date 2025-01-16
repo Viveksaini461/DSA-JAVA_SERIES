6c9a3496d94dcb8226735da24f209207c7c615d4
Viveksaini461 <150050925+Viveksaini461@users.noreply.github.com>
2025-01-16 20:37:15 +0530

    Update BubbleSort.java

--- Bubble Sort/BubbleSort.java
+++ Bubble Sort/BubbleSort.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 
 public class BubbleSort {
     public static void main(String[] args) {
-        int [] arr = {1,2,3,4,5};
+        int [] arr = {5,4,3,2,1};
         bubble(arr);
         System.out.println(Arrays.toString(arr));
     }
